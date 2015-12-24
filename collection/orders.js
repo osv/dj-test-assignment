643e3dbf65968955cc2256f5a0633b35c3f022dd
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-12-24 17:21:53 +0200

    fix type of client id and userid

--- collection/orders.js
+++ collection/orders.js
@@ -12,8 +12,30 @@ var schema = new SimpleSchema({
     type: String
   },
   client_id: {
-    type: Meteor.Collection.ObjectID
+    type: String,
+  },
+  userId: {                     // Owner of order
+    type: String,
   }
 });
 
 Orders.attachSchema(schema);
+
+Orders.allow({
+  insert: function (userId, doc) {
+    // The user must be logged in, and the document must be owned by the user
+    return (userId && doc.userId === userId);
+  },
+
+  update: function (userId, doc, fields, modifier) {
+    // can only change your own documents
+    return doc.userId === userId;
+  },
+
+  remove: function (userId, doc) {
+    // can only remove your own documents
+    return doc.userId === userId;
+  },
+
+  fetch: ['userId'],
+});
